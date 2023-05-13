3626ea8983fafd2e64906023880316565dabfdb0
DahlGitHub <78036458+DahlGitHub@users.noreply.github.com>
2023-05-14 01:12:29 +0200

    Update _document.tsx

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,15 +4,9 @@ export default function Document() {
   return (
     <Html lang='en'>
       <Head>
-        <link
-          rel='preload'
-          href='/fonts/inter-var-latin.woff2'
-          as='font'
-          type='font/woff2'
-          crossOrigin='anonymous'
-        />
+
       </Head>
-      <body>
+      <body className="bg-white text-black antialiased dark:bg-gray-900 dark:text-white">
         <Main />
         <NextScript />
       </body>
