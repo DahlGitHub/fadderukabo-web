9553d5ca54044dc08e23ccd6a3755c0091371319
DahlGitHub <78036458+DahlGitHub@users.noreply.github.com>
2023-05-14 02:10:59 +0200

    Adjusted signIn

--- src/components/auth/SignIn.tsx
+++ src/components/auth/SignIn.tsx
@@ -43,37 +43,50 @@ export default function SignIn() {
   }, [user, loading, router, auth]);
 
   return (
-    <section className="bg-gray-50 dark:bg-gray-900">
-  <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto lg:py-0">
-      <div className="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white">
-          <UsnIcon className="w-8 h-8 mr-2"/>
-          Universitet i Sørøst-Norge    
-      </div>
-      <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
-          <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
-              <h1 className="text-xl text-center font-semibold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
-                  Dashboard Login
-                 <div>
-        
-        <button
-          className="inline-flex items-center justify-center px-5 py-3 my-3 mx-auto w-full text-base font-medium text-center text-white rounded-lg bg-blue-600 hover:bg-blue-800 focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900"
-          onClick={signInWithGoogle}
-        >
-          <img
-            width={30}
-            src="https://www.freepnglogos.com/uploads/google-logo-png/google-logo-png-webinar-optimizing-for-success-google-business-webinar-13.png"
-          />
-          Login with Google
-        </button>
-        
-    </div> 
-              </h1>
-            
-          </div>
-      </div>
+<section className="bg-white dark:bg-gray-900 flex justify-center">
+  <div className="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6 ">
+
+
+          <div className="items-center bg-gray-50 rounded-lg shadow sm:flex dark:bg-gray-800 dark:border-gray-700">
+              <a href="#">
+                  <img className="rounded-lg sm:rounded-none sm:rounded-l-lg w-48" src="https://cdn.discordapp.com/attachments/1085273618763751434/1107094973926621294/rsz_6460246e1f0b7.png" alt="Jese Avatar"/>
+              </a>
+              <div className="p-5">
+                  <h3 className="text-xl font-bold tracking-tight text-gray-900 dark:text-white">
+                      <span>Dashboard</span>
+                  </h3>
+                  <p className="mt-3 mb-4 font-light text-gray-500 dark:text-gray-400">Jese drives the technical strategy.</p>
+                  <hr className="h-px my-8 bg-gray-200 border-0 dark:bg-gray-700"></hr>
+                  <div className="flex space-x-4 sm:mt-0">
+                  <button
+                className="inline-flex items-center justify-center px-5 w-48 py-2 w-full text-base font-medium text-center text-white rounded-lg bg-blue-600 hover:bg-blue-800 focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900"
+                onClick={signInWithGoogle}
+              >
+                <img className='mr-2'
+                  width={30}
+                  src="https://www.freepnglogos.com/uploads/google-logo-png/google-logo-png-webinar-optimizing-for-success-google-business-webinar-13.png"
+                />
+                <span className='text-[13px] font-bold'>Login with Google</span>
+              </button>
+                  </div>
+              </div>
+          </div> 
   </div>
-  <ToastContainer />
 </section>
+
+    /*
+<button
+                className="inline-flex items-center justify-center px-5 w-48 py-3 my-3 mx-auto w-full text-base font-medium text-center text-white rounded-lg bg-blue-600 hover:bg-blue-800 focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900"
+                onClick={signInWithGoogle}
+              >
+                <img className='mr-2'
+                  width={30}
+                  src="https://www.freepnglogos.com/uploads/google-logo-png/google-logo-png-webinar-optimizing-for-success-google-business-webinar-13.png"
+                />
+                <span className='text-[13px] font-bold'>Login with Google</span>
+              </button>
+*/
+
     
   );
 }
