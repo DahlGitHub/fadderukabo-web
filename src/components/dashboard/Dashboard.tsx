3203b032cefda699fc39e6c15cdae69a25a01817
DahlGitHub <78036458+DahlGitHub@users.noreply.github.com>
2023-05-15 10:24:51 +0200

    Added a temp dashboard

---
+++ src/components/dashboard/Dashboard.tsx
@@ -0,0 +1,20 @@
+import { collection, doc, getDoc, getDocs } from "firebase/firestore";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+import { auth} from "../../../firebase";
+import Link from "next/link";
+
+const Dashboard = () => {
+
+    const username = auth.currentUser?.displayName
+    const router = useRouter();
+
+    return(
+        <div className="bg-white dark:bg-gray-900 text-black dark:text-white h-[calc(100vh-70px)] ">
+            <div>Welcome {username}</div>
+            <Link href="./" onClick={() => auth.signOut()}>Sign out</Link>
+        </div>
+    )
+}
+
+export default Dashboard
---
+++ src/pages/dashboard/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Layout from '@/components/layout/Layout'
+import Dashboard from '@/components/dashboard/Dashboard'
+
+const index = () => {
+  return (
+    <Layout>
+      <Dashboard/>
+    </Layout>
+  )
+}
+
+export default index
