2201a3ce038309bbebd50ab2698ddcbb62a4cfb0
DahlGitHub <78036458+DahlGitHub@users.noreply.github.com>
2023-05-23 13:49:22 +0200

    Page

---
+++ src/pages/dashboard/program.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import DashboardLayout from '@/components/dashboard/DashboardLayout'
+import Program from '@/components/dashboard/Program'
+
+const program = () => {
+  return (
+    <DashboardLayout>
+      <Program/>
+    </DashboardLayout>
+  )
+}
+
+export default program
