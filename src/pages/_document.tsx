d8c8c20e47f60779c98d641f521f314d012b373f
DahlGitHub <78036458+DahlGitHub@users.noreply.github.com>
2023-05-23 13:48:51 +0200

    Imports

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,14 +1,19 @@
 import { Head, Html, Main, NextScript } from 'next/document';
+import 'flowbite';
 
 export default function Document() {
   return (
     <Html lang='en'>
       <Head>
+      <link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.6.5/flowbite.min.css"  rel="stylesheet" />
 
       </Head>
       <body className="bg-white text-black antialiased dark:bg-gray-900 dark:text-white">
         <Main />
         <NextScript />
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.6.5/flowbite.min.js"></script>
+
+
       </body>
     </Html>
   );
