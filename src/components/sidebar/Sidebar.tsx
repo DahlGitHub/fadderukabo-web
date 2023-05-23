bdf0c3cbb1b9064b3d518f90450add972df39eea
DahlGitHub <78036458+DahlGitHub@users.noreply.github.com>
2023-05-23 13:49:08 +0200

    Sidebar Menu

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -1,19 +1,24 @@
+import * as Collapsible from "@radix-ui/react-collapsible";
+import React, { useState } from "react";
 import SidebarItem from "./SidebarItem";
 
+export default function Sidebar() {
 
-const Sidebar = () => {
 
-  
   return (
-    <div className="flex h-[calc(100vh-70px)] transition-transform -translate-x-full bg-white border-r border-gray-200 sm:translate-x-0 dark:bg-gray-800 dark:border-gray-700" aria-label="Sidebar">
-    <div className="flex flex-col items-center space-y-10 py-2.5 p-2.5">
-        <div className=" rounded-md bg-white dark:bg-gray-800 ">
-        <SidebarItem/>
+    <>
+      <div>
+      <button data-drawer-target="default-sidebar" data-drawer-toggle="default-sidebar" aria-controls="default-sidebar" type="button" className="inline-flex items-center p-2 mt-2 ml-3 text-sm text-gray-500 rounded-lg sm:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600">
+   <span className="sr-only">Open sidebar</span>
+   <svg className="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+      <path clip-rule="evenodd" fill-rule="evenodd" d="M2 4.75A.75.75 0 012.75 4h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 4.75zm0 10.5a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5a.75.75 0 01-.75-.75zM2 10a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 10z"></path>
+   </svg>
+</button>
 
-        </div>
-    </div>
-    </div>
+<SidebarItem/>
+
+
+      </div>
+    </>
   );
-};
-
-export default Sidebar;
+}
