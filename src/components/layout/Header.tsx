d18e07b7486d6f18f661a3063a4aadde33896481
DahlGitHub <78036458+DahlGitHub@users.noreply.github.com>
2023-06-04 02:59:10 +0200

    Added icon

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,3 +1,4 @@
+import { AlignLeft} from "lucide-react"
 
 const Header = (props: {
     sidebarOpen: boolean;
@@ -18,9 +19,7 @@ const Header = (props: {
               className="z-50 block rounded-sm p-1.5 lg:hidden"
             >
               <span>
-              <svg className="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                <path clip-rule="evenodd" fill-rule="evenodd" d="M2 4.75A.75.75 0 012.75 4h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 4.75zm0 10.5a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5a.75.75 0 01-.75-.75zM2 10a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 10z"></path>
-              </svg>
+              <AlignLeft/>
               </span>
             </button>
             {/* <!-- Hamburger Toggle BTN --> */}
