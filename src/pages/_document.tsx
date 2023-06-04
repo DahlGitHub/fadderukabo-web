6abf3c4b1c32b1261d6a9edcb666b54cc00ae3c4
DahlGitHub <78036458+DahlGitHub@users.noreply.github.com>
2023-06-04 20:53:39 +0200

    removed flowbite

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,20 +1,34 @@
-import { Head, Html, Main, NextScript } from 'next/document';
-import 'flowbite';
+import React from "react";
+import Document, { Html, Head, Main, NextScript, DocumentContext } from "next/document";
 
-export default function Document() {
+class MyDocument extends Document {
+  static async getInitialProps(ctx : DocumentContext) {
+    const initialProps = await Document.getInitialProps(ctx);
+    return {
+      ...initialProps,
+      styles: React.Children.toArray([initialProps.styles]),
+    };
+  }
+
+render() {
   return (
     <Html lang='en'>
       <Head>
-      <link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.6.5/flowbite.min.css"  rel="stylesheet" />
+      {
+        //<link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.6.5/flowbite.min.css"  rel="stylesheet" />  
+      }
 
       </Head>
       <body className="bg-white text-black antialiased dark:bg-gray-900 dark:text-white">
         <Main />
         <NextScript />
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.6.5/flowbite.min.js"></script>
-
-
+        {
+          //<script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.6.5/flowbite.min.js"></script>
+        }
       </body>
     </Html>
   );
 }
+}
+
+export default MyDocument;
