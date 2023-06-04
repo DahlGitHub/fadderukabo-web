ec540c036cb5cbe4431cc6e15e72f41dd2923625
DahlGitHub <78036458+DahlGitHub@users.noreply.github.com>
2023-06-04 02:59:05 +0200

    Dashboard auth

--- src/components/dashboard/DashboardLayout.tsx
+++ src/components/dashboard/DashboardLayout.tsx
@@ -1,44 +1,63 @@
-import React, { ReactNode, useState } from "react";
+import React, { ReactNode, useEffect, useState } from "react";
 import Dashboard from "./Dashboard";
 import UserContext from "../auth/UserContext";
 import { auth } from "../../../firebase";
 import { useAuthState } from "react-firebase-hooks/auth";
 import Sidebar from "../sidebar/Sidebar";
 import Header from "../layout/Header";
+import { useRouter } from "next/router";
 
 interface DefaultLayoutProps {
   children: ReactNode;
 }
 
+
+
+
 const DashboardLayout = ({ children } : DefaultLayoutProps ) => {
+
+  const router = useRouter();
+  
+  const userLoggedIn = () => {
+  if(!auth.currentUser) {
+    router.push("/login")
+  }
+}
+
+useEffect(() => {
+  userLoggedIn()
+}, [])
+
+  const [user, loading, error] = useAuthState(auth);
   const [sidebarOpen, setSidebarOpen] = useState(false);
   return (
-
+    <UserContext.Provider value={{ user, loading, error }}>
     <div className="dark:bg-boxdark-2 dark:text-bodydark">
-      {/* <!-- ===== Page Wrapper Start ===== --> */}
+      { loading ? <div>Loading...</div> 
+      :
+
       <div className="flex h-screen overflow-hidden">
-        {/* <!-- ===== Sidebar Start ===== --> */}
+        
+
         <Sidebar sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
-        {/* <!-- ===== Sidebar End ===== --> */}
-
-        {/* <!-- ===== Content Area Start ===== --> */}
+  
         <div className="relative flex flex-1 flex-col overflow-y-auto overflow-x-hidden">
-          {/* <!-- ===== Header Start ===== --> */}
+   
           <Header sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
-          {/* <!-- ===== Header End ===== --> */}
-
-          {/* <!-- ===== Main Content Start ===== --> */}
+   
           <main>
             <div className="mx-auto max-w-screen-2xl p-4 md:p-6 2xl:p-10">
               {children}
             </div>
           </main>
-          {/* <!-- ===== Main Content End ===== --> */}
+      
         </div>
-        {/* <!-- ===== Content Area End ===== --> */}
+  
       </div>
-      {/* <!-- ===== Page Wrapper End ===== --> */}
+      }
+  
     </div>
+    </UserContext.Provider>
     
   );
 };
