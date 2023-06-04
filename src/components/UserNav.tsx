65305c08721195430f89405f0cf05852dbbf735f
DahlGitHub <78036458+DahlGitHub@users.noreply.github.com>
2023-06-04 20:53:47 +0200

    user nav for dashboard

---
+++ src/components/UserNav.tsx
@@ -0,0 +1,62 @@
+import { CreditCard, LogOut, PlusCircle, Settings, User } from "lucide-react"
+
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
+import { Button } from "@/components/ui/button"
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuShortcut,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+import { auth } from "../../firebase"
+import Link from "next/link"
+
+const signOut = () => {
+    
+ //auth.signOut().then(() => {
+      // toast.info("Signed out successfully");
+  //}
+  console.log("sign out"
+)}
+
+export function UserNav() {
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant="secondary" className="relative h-8 w-8 rounded-full">
+          <Avatar className="h-9 w-9">
+            <AvatarImage src={auth.currentUser?.photoURL ?? undefined} alt={auth.currentUser?.displayName ?? undefined} />
+          </Avatar>
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className="w-56" align="end" forceMount>
+        <DropdownMenuLabel className="font-normal">
+          <div className="flex flex-col space-y-1">
+            <p className="text-sm font-medium leading-none">{auth.currentUser?.displayName}</p>
+            <p className="text-xs leading-none text-muted-foreground">
+              {auth.currentUser?.email}
+            </p>
+          </div>
+        </DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        <DropdownMenuGroup>
+          <DropdownMenuItem>
+            <User className="mr-2 h-4 w-4" />
+            <span>Profile</span>
+          </DropdownMenuItem>
+        </DropdownMenuGroup>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem>
+        <Link href="./" onClick={signOut} className="hover:text-red-500 w-full flex">
+          <LogOut className="mr-2 h-4 w-4" />
+          <span>Sign out</span>
+        </Link>
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  )
+}
