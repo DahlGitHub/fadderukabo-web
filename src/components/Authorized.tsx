0ef5fca846bbd5f77db378e741e40acf85d0f07f
DahlGitHub <78036458+DahlGitHub@users.noreply.github.com>
2023-06-07 00:29:55 +0200

    see auth users and remove

--- src/components/Authorized.tsx
+++ src/components/Authorized.tsx
@@ -1,31 +1,95 @@
-import { collection, getDocs } from "firebase/firestore";
-import { db } from "../../firebase";
+import { collection, deleteDoc, doc, getDocs, query, where } from "firebase/firestore";
+import { auth, db } from "../../firebase";
 import { useEffect, useState } from "react";
+import AddAuthorized from "./AddAuthorized";
+import Confirmation from "./Confirmation";
 
 const Authorized = () => {
+  const [allowedEmails, setAllowedEmails] = useState<string[]>([]);
+  const [users, setUsers] = useState<any[]>([]);
 
-    const [allowedEmails, setAllowedEmails] = useState<string[]>([]);
+  useEffect(() => {
+    const fetchAllowedEmails = async () => {
+      const querySnapshot = await getDocs(collection(db, "allowedEmails"));
+      const emails = querySnapshot.docs.map((doc) => doc.data().email);
+      setAllowedEmails(emails);
+    };
 
-    useEffect(() => {
-      const fetchAllowedEmails = async () => {
-        const querySnapshot = await getDocs(collection(db, "allowedEmails"));
-        const emails = querySnapshot.docs.map((doc) => doc.data().email);
-        setAllowedEmails(emails);
-      };
+    fetchAllowedEmails();
+  }, []);
+
+  useEffect(() => {
+    const fetchUsers = async () => {
+      const q = query(collection(db, "users"), where("email", "in", allowedEmails));
+      const querySnapshot = await getDocs(q);
+      const usersData = querySnapshot.docs.map((doc) => doc.data());
+      setUsers(usersData);
+    };
+
+    if (allowedEmails.length > 0) {
+      fetchUsers();
+    }
+  }, [allowedEmails]);
+
+  const handleDeleteEmail = async (email: string) => {
+    try {
+      const currentUser = auth.currentUser;
   
-      fetchAllowedEmails();
-    }, []);
+      // Check if the current user is deleting their own email
+      if (currentUser && currentUser.email === email) {
+        console.log("Cannot delete your own email.");
+        return;
+      }
+  
+      // Check if the email is a protected email that should not be deleted
+      const protectedEmail = "fadderstyretbo@gmail.com";
+      if (email === protectedEmail) {
+        console.log(`Cannot delete the protected email: ${protectedEmail}`);
+        return;
+      }
+  
+      const querySnapshot = await getDocs(collection(db, "allowedEmails"));
+      const docToDelete = querySnapshot.docs.find((doc) => doc.data().email === email);
+  
+      if (docToDelete) {
+        await deleteDoc(doc(db, "allowedEmails", docToDelete.id));
+        setAllowedEmails((prevEmails) => prevEmails.filter((e) => e !== email));
+        console.log(`Email ${email} deleted successfully.`);
+      }
+    } catch (error) {
+      console.error("Error deleting email:", error);
+    }
+  };
 
-    return(
-        <div>
-            <h2>Add listed emails here</h2>
-            <ul className="text-[500px]">
-            {allowedEmails.map((email) => (
-            <li key={email}>{email}</li>
-            ))}
-            </ul>
-        </div>
-    )
-}
+  return (
+    <div>
+      <AddAuthorized />
+      <h2>List of Users with Allowed Emails</h2>
+      {users.length > 0 ? (
+        <ul className="text-sm">
+          {users.map((user) => (
+            <li key={user.email}>
+              <p>Name: {user.displayName}</p>
+              <p>Email: {user.email}</p>
+              <p>{user.created}</p>
+              <p>{user.signedIn}</p>
+              <img
+                src={user.photoURL}
+                alt={user.displayName}
+                className="w-10 h-10 rounded-full"  
+              />
+              <Confirmation 
+                onConfirm={() => handleDeleteEmail(user.email)} 
+                message={`Are you sure you want to delete ${user.email}?`}
+                />
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <p>No users found with allowed emails.</p>
+      )}
+    </div>
+  );
+};
 
-export default Authorized
+export default Authorized;
