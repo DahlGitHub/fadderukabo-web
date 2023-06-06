3739b5cdef908f3130ae93c5882dc29b39323d2b
DahlGitHub <78036458+DahlGitHub@users.noreply.github.com>
2023-06-07 00:30:02 +0200

    confirm delete dialog

---
+++ src/components/Confirmation.tsx
@@ -0,0 +1,60 @@
+import {
+    Dialog,
+    DialogContent,
+    DialogDescription,
+    DialogHeader,
+    DialogTitle,
+    DialogTrigger,
+    DialogFooter,
+  } from "@/components/ui/dialog"
+
+import { useState } from "react";
+
+interface ConfirmationProps {
+    onConfirm: () => void;
+    message: string;
+  }
+  
+  const Confirmation = ({ onConfirm, message }: ConfirmationProps) => {
+    const [isOpen, setIsOpen] = useState(false);
+  
+    const handleConfirm = () => {
+      onConfirm();
+      setIsOpen(false);
+    };
+  
+    const handleCancel = () => {
+      setIsOpen(false);
+    };
+  
+    return (
+      <div>
+        <Dialog>
+          <DialogTrigger onClick={() => setIsOpen(true)}>Delete</DialogTrigger>
+          <DialogContent>
+            <DialogHeader>
+              <DialogTitle>Are you sure absolutely sure?</DialogTitle>
+              <DialogDescription>
+                <p>{message}</p>
+                
+                </DialogDescription>
+                <DialogDescription className="pt-2">
+                    <p>This action cannot be undone.</p>
+                </DialogDescription>
+
+            </DialogHeader>
+            <div className="flex justify-between mt-4">
+              <button className="mr-2" onClick={handleCancel}>
+                Cancel
+              </button>
+              <button className="bg-red-500 text-white" onClick={handleConfirm}>
+                Confirm
+              </button>
+            </div>
+          </DialogContent>
+        </Dialog>
+      </div>
+    );
+  };
+  
+  export default Confirmation;
