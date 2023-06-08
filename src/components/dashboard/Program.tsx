90388b7c530bf79277cd4ee2cc3d0ad0ff5aff95
DahlGitHub <78036458+DahlGitHub@users.noreply.github.com>
2023-06-08 02:23:53 +0200

    Testpage

--- src/components/dashboard/Program.tsx
+++ src/components/dashboard/Program.tsx
@@ -1,25 +1,11 @@
-import { collection, doc, getDoc, getDocs } from "firebase/firestore";
-import { useRouter } from "next/router";
-import { useEffect, useState } from "react";
-import { auth} from "../../../firebase";
-import Link from "next/link";
-import { toast } from "react-toastify";
-import * as Avatar from '@radix-ui/react-avatar';
+import Page from "../authorized/Page";
 
 const Program = () => {
 
 
     return(
         <section className="bg-gray-50 dark:bg-gray-900 p-3 sm:p-5">
-            <div className="min-h-screen bg-gray-200">
-                Hi
-            </div>
-            <div className="min-h-screen bg-gray-300">
-                Hi
-            </div>
-            <div className="min-h-screen bg-gray-400">
-                Hi
-            </div>
+            <Page />
         </section>
     )
 }
