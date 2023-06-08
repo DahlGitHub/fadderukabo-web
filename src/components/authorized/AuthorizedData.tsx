cdb0e47b2d883e6231efdce41afac07efa7e2726
DahlGitHub <78036458+DahlGitHub@users.noreply.github.com>
2023-06-08 02:23:32 +0200

    Auth datatable

---
+++ src/components/authorized/AuthorizedData.tsx
@@ -0,0 +1,205 @@
+"use client"
+
+import {ColumnDef,flexRender,getCoreRowModel,useReactTable,} from "@tanstack/react-table"
+import {Table,TableBody,TableCell,TableHead,TableHeader,TableRow,} from "@/components/ui/table"
+import { MoreHorizontal } from "lucide-react"
+import { Button } from "@/components/ui/button"
+import {DropdownMenu,DropdownMenuContent,DropdownMenuItem,DropdownMenuLabel,DropdownMenuSeparator,DropdownMenuTrigger,} from "@/components/ui/dropdown-menu"
+import moment from "moment"
+import Confirmation from "../Confirmation"
+import { collection, deleteDoc, doc, getDocs } from "firebase/firestore"
+import { auth, db } from "../../../firebase"
+
+
+export type Authorized = {
+    id: string
+    photoURL: string
+    displayName: string
+    email: string
+    created: number
+    signedIn: number
+  }
+
+  const handleDeleteEmail = async (email: string): Promise<void> => {
+    try {
+      const currentUser = auth.currentUser;
+
+      // Check if the current user is deleting their own email
+      if (currentUser && currentUser.email === email) {
+        console.log("Cannot delete your own email.");
+        return;
+      }
+
+      // Check if the email is a protected email that should not be deleted
+      const protectedEmail = "fadderstyretbo@gmail.com";
+      if (email === protectedEmail) {
+        console.log(`Cannot delete the protected email: ${protectedEmail}`);
+        return;
+      }
+
+      const querySnapshot = await getDocs(collection(db, "allowedEmails"));
+      const docToDelete = querySnapshot.docs.find((doc) => doc.data().email === email);
+
+      if (docToDelete) {
+        await deleteDoc(doc(db, "allowedEmails", docToDelete.id));
+      }
+    } catch (error) {
+      console.error("Error deleting email:", error);
+    }
+  };
+
+  export const columns: ColumnDef<Authorized>[] = [
+    {
+    accessorKey: "name",
+    header: "Name",
+    cell: ({ row }) => {
+        const authorized = row.original
+        return (
+          <div className="flex items-center">
+            <img
+              src={authorized.photoURL}
+              alt={authorized.displayName}
+              className="w-8 h-8 rounded-full"
+            />
+            <span className="ml-2">{authorized.displayName}</span>
+          </div>
+        )
+      }
+    },
+    {
+      accessorKey: "email",
+      header: "Email",
+    },
+    {
+      accessorKey: "created",
+      header: "Created",
+        cell: ({ row }) => {
+            const created = row.getValue("created")
+            const date = moment(created as number)
+    
+            return (
+              <div className="flex items-center">
+                <span >{date.format('MMM D, YYYY')}</span>
+              </div>
+            )
+          }
+    },
+    {
+    accessorKey: "signedIn",
+    header: "Last signed in",
+    cell: ({ row }) => {
+        const signedIn = row.getValue("signedIn")
+        const date = moment(signedIn as number)
+
+        return (
+          <div className="flex items-center">
+            <span >{date.fromNow()}</span>
+          </div>
+        )
+      }
+    },
+    {
+        id: "actions",
+        cell: ({ row }) => {
+          
+     
+          return (
+            <DropdownMenu>
+              <DropdownMenuTrigger asChild>
+                <Button variant="ghost" className="h-8 w-8 p-0">
+                  <span className="sr-only">Open menu</span>
+                  <MoreHorizontal className="h-4 w-4" />
+                </Button>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent align="end">
+                <DropdownMenuLabel>Actions</DropdownMenuLabel>
+
+                <DropdownMenuSeparator />
+                {
+                /*
+                           <DropdownMenuItem
+                    onClick={() => handleDeleteEmail(row.getValue("email"))}>
+                        Delete
+                </DropdownMenuItem>
+                 */   
+                }
+                <DropdownMenuItem asChild>
+                    
+                    <Confirmation 
+                        onConfirm={() => handleDeleteEmail(row.getValue("email"))} 
+                        message={`Are you sure you want to delete ${row.getValue("email")}?`}
+                    />
+                       
+ 
+                </DropdownMenuItem>
+                <DropdownMenuItem>View payment details</DropdownMenuItem>
+              </DropdownMenuContent>
+            </DropdownMenu>
+          )
+        },
+      },
+  ]
+
+interface DataTableProps<TData, TValue> {
+    columns: ColumnDef<TData, TValue>[]
+    data: TData[]
+}
+
+export function DataTable<TData, TValue>({
+    columns,
+    data,
+  }: DataTableProps<TData, TValue>) {
+    const table = useReactTable({
+      data,
+      columns,
+      getCoreRowModel: getCoreRowModel(),
+    })
+   
+    return (
+      <div className="rounded-md border">
+        <Table>
+          <TableHeader>
+            {table.getHeaderGroups().map((headerGroup) => (
+              <TableRow key={headerGroup.id}>
+                {headerGroup.headers.map((header) => {
+                  return (
+                    <TableHead key={header.id}>
+                      {header.isPlaceholder
+                        ? null
+                        : flexRender(
+                            header.column.columnDef.header,
+                            header.getContext()
+                          )}
+                    </TableHead>
+                  )
+                })}
+              </TableRow>
+            ))}
+          </TableHeader>
+          <TableBody>
+            {table.getRowModel().rows?.length ? (
+              table.getRowModel().rows.map((row) => (
+                <TableRow
+                  key={row.id}
+                  data-state={row.getIsSelected() && "selected"}
+                >
+                  {row.getVisibleCells().map((cell) => (
+                    <TableCell key={cell.id}>
+                      {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                    </TableCell>
+                  ))}
+                </TableRow>
+              ))
+            ) : (
+              <TableRow>
+                <TableCell colSpan={columns.length} className="h-24 text-center">
+                  No results.
+                </TableCell>
+              </TableRow>
+            )}
+          </TableBody>
+        </Table>
+      </div>
+    )
+  }
+
