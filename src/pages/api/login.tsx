441b9f29a48d2d5c80a64da6eec4edc96b4113fa
DahlGitHub <78036458+DahlGitHub@users.noreply.github.com>
2023-06-13 01:47:06 +0200

    firebase-auth

---
+++ src/pages/api/login.tsx
@@ -0,0 +1,18 @@
+import firebase from "firebase-admin"
+
+import initAuth from '../../components/auth/initAuth' // the module you created above
+import { setAuthCookies } from "next-firebase-auth";
+import { NextApiRequest, NextApiResponse } from "next";
+
+initAuth()
+
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+  try {
+    await setAuthCookies(req, res)
+  } catch (e) {
+    return res.status(500).json({ error: 'Unexpected error.'} )
+  }
+  return res.status(200).json({ success: true })
+}
+
+export default handler
