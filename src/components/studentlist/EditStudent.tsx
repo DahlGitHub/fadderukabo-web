ba139ed1ac62435ae1a87baa9b8ab96bb1adde17
DahlGitHub <78036458+DahlGitHub@users.noreply.github.com>
2023-06-14 00:49:58 +0200

    edit student data

--- src/components/studentlist/EditStudent.tsx
+++ src/components/studentlist/EditStudent.tsx
@@ -1,56 +1,166 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Authorized } from './StudentData';
 import {
     Dialog,
     DialogContent,
-    DialogDescription,
-    DialogHeader,
-    DialogTitle,
     DialogTrigger,
   } from "@/components/ui/dialog"
-import { doc, updateDoc } from 'firebase/firestore';
+import { collection, doc, onSnapshot, updateDoc } from 'firebase/firestore';
 import { db } from '../../../firebase';
+
+import { Button } from "@/components/ui/button"
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+
+import * as z from "zod"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from 'react-hook-form';
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/select';
 
 interface EditDataProps {
   data: Authorized;
   docId: string;
 }
 
+const FormSchema = z.object({
+    name: z.string().min(2, {
+      message: "Name must be at least 2 characters.",
+    }),
+    group: z.string().min(2, {
+        message: "Status must be at least 2 characters.",
+        }),
+    status: z.string().min(2, {
+        message: "Status must be at least 2 characters.",
+    }),
+  })
+
+  
 export const EditStudent: React.FC<EditDataProps> = ({ docId, data}) => {
     const [currentData, setCurrentData] = React.useState(data);
+    const [groupOptions, setGroupOptions] = useState<string[]>([]);
     const [isOpen, setIsOpen] = useState(false);
 
-    const handleSubmit = async (event: React.FormEvent) => {
-        event.preventDefault();
-        await updateDoc(doc(db, 'studentdata', docId), currentData);
+    useEffect(() => {
+        const unsubscribe = onSnapshot(collection(db, 'groupdata'), (snapshot) => {
+          const titles = snapshot.docs.map((doc) => doc.data().title);
+          setGroupOptions(titles);
+        });
+    
+        return () => unsubscribe();
+      }, []);
+    
+
+    const form = useForm<z.infer<typeof FormSchema>>({
+        defaultValues: {
+            name: currentData.name,
+            group: currentData.group,
+            status: currentData.status,
+        },
+        resolver: zodResolver(FormSchema),
+      })
+
+    function onSubmit(data: z.infer<typeof FormSchema>) {
+        console.log(data);
+        updateDoc(doc(db, "studentdata", docId), {
+            name: data.name,
+            group: data.group,
+            status: data.status,
+          });
         setIsOpen(false);
-      };
+      }
 
   return (
     <Dialog>
         <DialogTrigger onClick={() => setIsOpen(true)}>EditThis</DialogTrigger>
         {isOpen && (
         <DialogContent>
-          <form onSubmit={handleSubmit}>
-            <input
-              type="text"
-              value={currentData.name}
-              onChange={(e) =>
-                setCurrentData({ ...currentData, name: e.target.value })
-              }
-            />
-        <select
-          value={currentData.status}
-          onChange={(e) =>
-            setCurrentData({ ...currentData, status: e.target.value as "fadder" | "faddersjef" })
-          }
-        >
-          <option value="fadder">Fadder</option>
-          <option value="faddersjef">Faddersjef</option>
-        </select>
-            {/* Add more inputs for each field you want to edit */}
-            <button type="submit">Save</button>
-          </form>
+            <Form {...form}>
+                <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+                    <FormField
+                    control={form.control}
+                    name="name"
+                    render={({ field }) => (
+                        <FormItem>
+                            <FormLabel>Name</FormLabel>
+                            <FormControl>
+                                <Input {...field} defaultValue={currentData.name} />
+                            </FormControl>
+                            <FormDescription>
+                                Fullname of the person.
+                            </FormDescription>
+                            <FormMessage />
+                        </FormItem>
+                        
+                    )}
+                    />
+                    <FormField
+                    control={form.control}
+                    name="group"
+                    render={({ field }) => (
+                        <FormItem>
+                            <FormLabel>Group</FormLabel>
+                            <Select
+                                onValueChange={field.onChange}
+                                defaultValue={currentData.group}
+                            >
+                            <FormControl>
+                            <SelectTrigger>
+                            <SelectValue placeholder="Select a group" />
+                            </SelectTrigger>
+                            </FormControl>
+                            <SelectContent>
+                                {groupOptions.map((group) => (
+                                <SelectItem key={group} value={group}>
+                                    {group}
+                                </SelectItem>
+                                ))}
+                            </SelectContent>
+                             </Select>
+                            <FormDescription>
+                                Group section for the person.
+                            </FormDescription>
+                            <FormMessage />
+                        </FormItem>
+                        
+                    )}
+                    />
+                    <FormField
+                    control={form.control}
+                    name="status"
+                    render={({ field }) => (
+                        <FormItem>
+                            <FormLabel>Status</FormLabel>
+                            <Select onValueChange={field.onChange} defaultValue={currentData.status}>
+                                <FormControl>
+                                <SelectTrigger>
+                                    <SelectValue placeholder="Select a status" />
+                                </SelectTrigger>                            
+                                </FormControl>
+                                <SelectContent>
+                                    <SelectItem value="fadder">Fadder</SelectItem>
+                                    <SelectItem value="faddersjef">Faddersjef</SelectItem>
+                                </SelectContent>
+                            </Select>
+                            <FormDescription>
+                                Fadderstatus for the person.
+                            </FormDescription>
+                            <FormMessage />
+                        </FormItem>
+                        
+                    )}
+                    />
+                    <Button type="submit">Update</Button>
+                </form>
+            </Form>
+
           <DialogTrigger onClick={() => setIsOpen(false)}>Cancel</DialogTrigger>
         </DialogContent>
       )}
