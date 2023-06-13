ad7b09ff4d8abaaef54c86d9a0a872f8302f94cb
DahlGitHub <78036458+DahlGitHub@users.noreply.github.com>
2023-06-14 00:50:20 +0200

    studentlist page

---
+++ src/components/studentlist/StudentListPage.tsx
@@ -0,0 +1,30 @@
+import { useEffect, useState } from "react";
+import { collection, onSnapshot, doc, updateDoc } from "firebase/firestore";
+import { auth, db } from "../../../firebase";
+import { columns, DataTable, Authorized } from "./StudentData";
+
+
+export default function StudentListPage() {
+  
+  const [data, setData] = useState<Authorized[]>([]);
+
+  useEffect(() => {
+    const unsubscribe = onSnapshot(collection(db, 'studentdata'), (snapshot) => {
+      const newData = snapshot.docs.map((doc) => ({
+        docId: doc.id,
+        ...doc.data(),
+      }) as Authorized);
+      setData(newData);
+    });
+  
+    // Detach the listener when the component unmounts
+    return () => unsubscribe();
+  }, []);
+  
+
+    return (
+      <div className="container mx-auto py-10">
+        <DataTable columns={columns} data={data}  />
+      </div>
+    );
+  }
