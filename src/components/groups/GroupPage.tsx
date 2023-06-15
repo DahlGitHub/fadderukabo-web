b4d89821dcae630d7d44fcc677cfeb079cf9c925
DahlGitHub <78036458+DahlGitHub@users.noreply.github.com>
2023-06-15 22:35:04 +0200

    add grouppage

---
+++ src/components/groups/GroupPage.tsx
@@ -0,0 +1,32 @@
+import { useEffect, useState } from "react";
+import { collection, onSnapshot, doc, updateDoc } from "firebase/firestore";
+import { auth, db } from "../../../firebase";
+import { columns, DataTable, Group } from "./GroupData";
+import AddGroup from "./AddGroup";
+
+
+export default function StudentListPage() {
+  
+  const [data, setData] = useState<Group[]>([]);
+
+  useEffect(() => {
+    const unsubscribe = onSnapshot(collection(db, 'groupdata'), (snapshot) => {
+      const newData = snapshot.docs.map((doc) => ({
+        docId: doc.id,
+        ...doc.data(),
+      }) as Group);
+      setData(newData);
+    });
+  
+    // Detach the listener when the component unmounts
+    return () => unsubscribe();
+  }, []);
+  
+
+    return (
+      <div className="container mx-auto py-10">
+        <AddGroup />
+        <DataTable columns={columns} data={data}  />
+      </div>
+    );
+  }
--- src/pages/dashboard/groups.tsx
+++ src/pages/dashboard/groups.tsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import DashboardLayout from '@/components/dashboard/DashboardLayout'
 import { AuthAction, withAuthUser, withAuthUserTokenSSR } from 'next-firebase-auth'
+import GroupPage from '@/components/groups/GroupPage'
 
 
 const groups = () => {
   return (
     <DashboardLayout>
-      
+      <GroupPage/>
     </DashboardLayout>
   )
 }
