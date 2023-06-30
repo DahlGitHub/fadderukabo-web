70e269d16182c2bdc60e270c889e7943af36137a
DahlGitHub <78036458+DahlGitHub@users.noreply.github.com>
2023-06-30 03:39:28 +0200

    SelectFilter

---
+++ src/components/studentlist/StudentFacetedFilter.tsx
@@ -0,0 +1,163 @@
+import * as React from "react"
+import { Column } from "@tanstack/react-table"
+import { Check, HelpCircle, LucideIcon, PlusCircle } from "lucide-react"
+
+import { cn } from "@/lib/utils"
+import { Popover, PopoverContent, PopoverTrigger } from "../ui/popover"
+import { Button } from "../ui/button"
+import { Badge } from "../ui/badge"
+import { Separator } from "../ui/separator"
+import { Command, CommandEmpty, CommandGroup, CommandInput, CommandItem, CommandList, CommandSeparator } from "../ui/command"
+import { useEffect, useState } from "react"
+import { collection, getDocs } from "firebase/firestore"
+import { db } from "../../../firebase"
+
+
+interface StudentFacetedFilter<TData, TValue> {
+  column?: Column<TData, TValue>
+  title?: string
+  options: {
+    label: string
+    value: string
+    color?: string
+  }[]
+}
+
+export const groupSelections: GroupSelection[] = [];
+
+interface GroupSelection {
+  label: string;
+  value: string;
+  color?: string;
+}
+
+
+export async function fetchGroupSelections(): Promise<void> {
+  const groupData = await getDocs(collection(db, "groupdata"));
+  const fetchedGroupSelections: GroupSelection[] = groupData.docs.map((doc) => {
+    const data = doc.data();
+    return { label: data.title, value: data.title, color: data.hexValue };
+  });
+  groupSelections.length = 0; // Clear the existing array
+  groupSelections.push(...fetchedGroupSelections); // Push the fetched data into the array
+}
+
+// Call the fetchGroupSelections function to fetch the data and update the groupSelections array
+fetchGroupSelections();
+
+export function StudentFacetedFilter<TData, TValue>({
+  column,
+  title,
+  options,
+}: StudentFacetedFilter<TData, TValue>) {
+  const facets = column?.getFacetedUniqueValues()
+  const selectedValues = new Set(column?.getFilterValue() as string[])
+
+
+  return (
+    <Popover>
+      <PopoverTrigger asChild>
+        <Button variant="outline" size="sm" className="h-8 border-dashed">
+          <PlusCircle className="mr-2 h-4 w-4" />
+          {title}
+          {selectedValues?.size > 0 && (
+            <>
+              <Separator orientation="vertical" className="mx-2 h-4" />
+              <Badge
+                variant="secondary"
+                className="rounded-sm px-1 font-normal lg:hidden"
+              >
+                {selectedValues.size}
+              </Badge>
+              <div className="hidden space-x-1 lg:flex">
+                {selectedValues.size > 2 ? (
+                  <Badge
+                    variant="secondary"
+                    className="rounded-sm px-1 font-normal"
+                  >
+                    {selectedValues.size} selected
+                  </Badge>
+                ) : (
+                  options
+                    .filter((option) => selectedValues.has(option.value))
+                    .map((option) => (
+                      <Badge
+                        variant="secondary"
+                        key={option.value}
+                        className="rounded-sm px-1 font-normal"
+                      >
+                        <div className="mr-1 rounded-full w-2.5 h-2.5" style={{backgroundColor: option.color }}></div>
+                        {option.label}
+                      </Badge>
+                    ))
+                )}
+              </div>
+            </>
+          )}
+        </Button>
+      </PopoverTrigger>
+      <PopoverContent className="w-[200px] p-0" align="start">
+        <Command>
+          
+          <CommandList>
+            <CommandEmpty>No results found.</CommandEmpty>
+            <CommandGroup>
+              {options.map((option) => {
+                const isSelected = selectedValues.has(option.value)
+                return (
+                  <CommandItem
+                    key={option.value}
+                    onSelect={() => {
+                      if (isSelected) {
+                        selectedValues.delete(option.value)
+                      } else {
+                        selectedValues.add(option.value)
+                      }
+                      const filterValues = Array.from(selectedValues)
+                      column?.setFilterValue(
+                        filterValues.length ? filterValues : undefined
+                      )
+                    }}
+                  >
+                    <div
+                      className={cn(
+                        "mr-2 flex h-4 w-4 items-center justify-center rounded-sm border border-primary",
+                        isSelected
+                          ? "bg-primary text-primary-foreground"
+                          : "opacity-50 [&_svg]:invisible"
+                      )}
+                    >
+                      <Check className={cn("h-4 w-4")} />
+                    </div>
+                    <div className="mr-2 flex items-center">
+                    <div className="rounded-full w-3 h-3" style={{backgroundColor: option.color }}></div>
+                    </div>
+                    <span className="text-xs truncate">{option.label}</span>
+                    {facets?.get(option.value) && (
+                      <span className="ml-auto flex h-4 w-4 items-center justify-center font-mono text-xs">
+                        {facets.get(option.value)}
+                      </span>
+                    )}
+                  </CommandItem>
+                )
+              })}
+            </CommandGroup>
+            {selectedValues.size > 0 && (
+              <>
+                <CommandSeparator />
+                <CommandGroup>
+                  <CommandItem
+                    onSelect={() => column?.setFilterValue(undefined)}
+                    className="justify-center text-center"
+                  >
+                    Clear filters
+                  </CommandItem>
+                </CommandGroup>
+              </>
+            )}
+          </CommandList>
+        </Command>
+      </PopoverContent>
+    </Popover>
+  )
+}
