9d3866a18dd1b9ef18461b8547e3c4d2f0f7c5b3
DahlGitHub <78036458+DahlGitHub@users.noreply.github.com>
2023-07-07 05:38:27 +0200

    Update groups.tsx

--- src/pages/dashboard/groups.tsx
+++ src/pages/dashboard/groups.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import DashboardLayout from '@/components/dashboard/DashboardLayout';
 import {
   AuthAction,
@@ -6,11 +6,62 @@ import {
   withAuthUserTokenSSR,
 } from 'next-firebase-auth';
 import GroupPage from '@/components/groups/GroupPage';
+import { DataTable, Group, columns } from '@/components/groups/GroupData';
+import { onSnapshot, collection, query, where } from 'firebase/firestore';
+import { db } from '../../../firebase';
+import { TableSkeleton } from '@/components/TableSkeleton';
 
 const groups = () => {
+  const [data, setData] = useState<Group[]>([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    onSnapshot(collection(db, 'groupdata'), snapshot => {
+      const fetchedGroups = snapshot.docs.map(doc => ({
+        docId: doc.id,
+        ...doc.data(),
+      })) as Group[];
+
+      // For each fetched group, fetch member count and create a new group object
+      // that includes the member count
+      const updatedGroups: Group[] = [];
+
+      fetchedGroups.forEach(group => {
+        const querySnapshot = query(
+          collection(db, 'studentdata'),
+          where('group', '==', group.title),
+        );
+
+        const unsubscribe = onSnapshot(querySnapshot, snapshot => {
+          updatedGroups.push({ ...group, members: snapshot.size });
+
+          // Check if we've updated all groups, if so, set the data state
+          if (updatedGroups.length === fetchedGroups.length) {
+            setData(updatedGroups);
+            setIsLoading(false);
+          }
+        });
+
+        return () => {
+          unsubscribe();
+        };
+      });
+    });
+  }, []);
+
   return (
     <DashboardLayout>
-      <GroupPage />
+      <div className="pb-5">
+        <h2 className="text-2xl font-bold tracking-tight">Groups</h2>
+        <p className="text-muted-foreground">
+          List of people in need of medical assistance
+        </p>
+      </div>
+      {isLoading ? (
+        <TableSkeleton columnCount={4} />
+      ) : (
+        <DataTable columns={columns} data={data} />
+      )}
     </DashboardLayout>
   );
 };
