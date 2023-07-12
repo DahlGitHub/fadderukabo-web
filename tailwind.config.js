3609a0c2bc72b78a149f553ead0c48809b2b98f4
DahlGitHub <78036458+DahlGitHub@users.noreply.github.com>
2023-07-12 03:36:56 +0200

    imports

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,6 +16,9 @@ module.exports = {
       },
     },
     extend: {
+      fontFamily: {
+        poppins: ['Poppins'],
+      },
       colors: {
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
