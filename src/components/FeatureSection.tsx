0eb60083aa5d506b9f6293cbf3325acb3f6487a3
DahlGitHub <78036458+DahlGitHub@users.noreply.github.com>
2023-07-22 22:34:09 +0200

    Update FeatureSection.tsx

--- src/components/FeatureSection.tsx
+++ src/components/FeatureSection.tsx
@@ -10,6 +10,10 @@ interface FeatureSectionProps {
   title: string;
   sectionNumber: string;
   color: string;
+  textColor: string;
+  titleColor: string;
+  bgColor: string;
+  iconBgColor: string;
   features: Feature[];
 }
 
@@ -17,30 +21,62 @@ const FeatureSection: React.FC<FeatureSectionProps> = ({
   title,
   sectionNumber,
   features,
-  color
+  color,
+  bgColor,
+  textColor,
+  titleColor,
+  iconBgColor,
 }) => {
   return (
-    <div className="container py-5 pb-5 bg-gray-800">
-      <section className="dark:bg-gray-900">
+    <div className={`py-5 pb-5 ${bgColor}`}>
+      <section className="container">
         <div className="py-8 sm:py-16 lg:px-6">
           <div className="pb-5 container flex flex-col font-poppins text-center">
             <span className={`font-semibold text-2xl ${color}`}>
               {sectionNumber}
             </span>
-            <span className="text-4xl font-bold text-gray-900">{title}</span>
+            <h2 className="max-w-lg mb-6 font-sans text-4xl font-bold text-gray-900 sm:text-4xl md:mx-auto">
+          <span className="relative inline-block">
+            <svg
+              viewBox="0 0 52 24"
+              fill="white"
+              className="absolute top-0 left-0 z-0 hidden w-32 -mt-8 -ml-20 text-blue-gray-100 lg:w-32 lg:-ml-28 lg:-mt-10 sm:block"
+            >
+              <defs>
+                <pattern
+                  id="ea469ae8-e6ec-4aca-8875-fc402da4d16e"
+                  x="0"
+                  y="0"
+                  width=".135"
+                  height=".30"
+                >
+                  <circle cx="1" cy="1" r=".7" />
+                </pattern>
+              </defs>
+              <rect
+                fill="url(#ea469ae8-e6ec-4aca-8875-fc402da4d16e)"
+                width="52"
+                height="24"
+              />
+            </svg>
+            <span className={`relative ml-5 font-poppins ${titleColor}`}>{title}</span>
+          </span>{' '}
+        </h2>
           </div>
           <div className="space-y-8 md:grid my-5 md:grid-cols-2 lg:grid-cols-3 gap-5 md:space-y-0 font-poppins">
             {features.map((feature) => (
-              <div className="bg-gray-100 p-4 rounded-lg" key={feature.title}>
-                <div className="flex mb-4">
-                  <span className="bg-white p-2 rounded-lg">{feature.icon}</span>
+              <div className="p-4" key={feature.title}>
+                <div className="flex mb-4 justify-center">
+                  <span className={`p-4 rounded-full ${iconBgColor}`}>{feature.icon}</span>
                 </div>
-                <h3 className="mb-2 text-lg font-semibold text-gray-800">
+                <div className="text-center">
+                <h3 className={`mb-2 text-lg font-semibold text-gray-800 ${titleColor}`}>
                   {feature.title}
                 </h3>
-                <p className="text-gray-500">
+                <p className={`text-gray-500 ${textColor} mx-20`}>
                   {feature.description}
                 </p>
+                </div>
               </div>
             ))}
           </div>
