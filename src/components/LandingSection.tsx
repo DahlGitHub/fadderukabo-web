483caddaf35c4747c2785c1cbf82cccb7dc13172
DahlGitHub <78036458+DahlGitHub@users.noreply.github.com>
2023-07-22 22:34:04 +0200

    frontpage

--- src/components/LandingSection.tsx
+++ src/components/LandingSection.tsx
@@ -1,8 +1,51 @@
 import Link from 'next/link';
 import { Button } from './ui/button';
 import { CountDown } from './CountDown';
+import { useEffect, useState } from 'react';
+import { Timestamp, doc, getDoc } from 'firebase/firestore';
+import { db } from '../../firebase';
+import { ChevronRight } from 'lucide-react';
 
 export const LandingSection = () => {
+  const [targetDate, setTargetDate] = useState(null);
+
+  useEffect(() => {
+    // Fetch the target date from the database.
+    const fetchTargetDate = async () => {
+      try {
+        const docRef = doc(db, 'timerdate', 'timer');
+        const docSnap = await getDoc(docRef);
+        if (docSnap.exists()) {
+          const data = docSnap.data();
+          // Assuming your target date is stored in a field called 'targetDate'.
+          const targetDateFromDB = data.date;
+          setTargetDate(targetDateFromDB);
+        }
+      } catch (error) {
+        console.error('Error fetching target date:', error);
+      }
+    };
+
+    fetchTargetDate();
+  }, []);
+
+  const formatTargetDate = (timestamp: Timestamp) => {
+    if (!timestamp) return ''; // Return empty string if targetDate is not set
+
+    const startDate = timestamp.toDate();
+    const endDate = new Date(startDate.getTime() + 6 * 24 * 60 * 60 * 1000); // Adding 7 days to the startDate
+    const formattedStartDate = startDate.toLocaleString('nb-NO', {
+      day: 'numeric',
+    });
+    const formattedEndDate = endDate.toLocaleString('nb-NO', {
+      day: 'numeric',
+      month: 'short',
+    });
+    const formattedDateRange = `${formattedStartDate} - ${formattedEndDate}`;
+
+    return formattedDateRange;
+  };
+
   return (
     <div className="container py-5 pb-5 min-h-screen">
       <div
@@ -16,21 +59,32 @@ export const LandingSection = () => {
         <div className="md:w-1/2">
           <div className="pb-5 container flex flex-col font-poppins">
             <span className="font-semibold text-2xl text-purple-600">
-              13. - 20. august
+              {targetDate && (
+                <span className="font-semibold text-2xl text-purple-600">
+                  {formatTargetDate(targetDate)}
+                </span>
+              )}
             </span>
             <span className="text-5xl font-bold text-gray-900">
               Fadderuka Bø
             </span>
-            <div className="py-5 text-gray-800 tracking-wide leading-6.5 break-normal">
-              <CountDown />
+            <div className="py-5 text-gray-800 tracking-wide leading-6.5 break-normal text-center">
+              {targetDate && <CountDown targetDate={targetDate} />}
             </div>
-            <Link href="/program" className="z-10">
-              <Button variant="default">Program</Button>
+            <div className='text-center z-10'>
+            <Link href="/program">
+              <Button variant="default" size={"lg"}>Program <ChevronRight className="inline ml-2 transition-transform hover:scale-110" /></Button>
             </Link>
+            </div>
           </div>
         </div>
         <div className="md:w-2/3 container">
-          <img src={'https://firebasestorage.googleapis.com/v0/b/fadderukabo.appspot.com/o/usnfadder.jpg?alt=media&token=0c000d64-6a89-46ee-af3f-58e08cd73883'} className="w-full rounded" />
+          <img
+            src={
+              'https://firebasestorage.googleapis.com/v0/b/fadderukabo.appspot.com/o/usnfadder.jpg?alt=media&token=0c000d64-6a89-46ee-af3f-58e08cd73883'
+            }
+            className="w-full rounded"
+          />
         </div>
       </div>
     </div>
