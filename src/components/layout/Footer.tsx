7f0019578f0d788db63d4b1612b8ef9a37044f1d
DahlGitHub <78036458+DahlGitHub@users.noreply.github.com>
2023-07-22 22:33:33 +0200

    Update Footer.tsx

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,24 +1,25 @@
 import * as React from 'react';
 import Image from 'next/image';
 import FooterItems from '../FooterItems';
+import Link from 'next/link';
 
 export default function Footer() {
   return (
     <footer className="bg-gray-100">
-      <div className="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8">
+      <div className="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8 font-poppins">
       <FooterItems />
         <div className='py-2 text-xs text-center'>
-          Copyright @ 2023 Adrian Dahl
+          Copyright @ 2023 <Link href="https://www.linkedin.com/in/dahladrian/" className='hover:underline' target='_blank'>Adrian Dahl</Link> 
         </div>
         <hr className="my-2 border-gray-200" />
         <div className="pt-3 flex items-center space-x-2">
           <Image
-            src="https://firebasestorage.googleapis.com/v0/b/fadderukabo.appspot.com/o/USN-symbol_lilla.png?alt=media&token=0ab4e9b3-3974-41f4-8d7f-41a0753bed07"
+            src="https://firebasestorage.googleapis.com/v0/b/fadderukabo.appspot.com/o/USN-symbol_sort.png?alt=media&token=e72960d6-666d-4c3c-9b6a-a38d9ab25ce7"
             alt={'USN'}
             width={25}
             height={25}
           />
-          <span className=''>Universitetet i Sørøst-Norge</span>
+          <span className='text-sm'>Universitetet i Sørøst-Norge</span>
         </div>
       </div>
     </footer>
