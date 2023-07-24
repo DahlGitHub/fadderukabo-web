636ae2517b7129a7d9ec3773bb03276622e93dee
DahlGitHub <78036458+DahlGitHub@users.noreply.github.com>
2023-07-24 22:18:25 +0200

    back to dropdownmenu

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -14,53 +14,103 @@ import {
   navigationMenuTriggerStyle,
 } from '@/components/ui/navigation-menu';
 
-const components: {
+const menuItems: {
   title: string;
   href: string;
   description: string;
-  id?: string;
+  category: string;
 }[] = [
   {
-    title: 'FAQ',
-    href: '/',
-    description:
-      'Coming soon.',
-    id: '',
+    title: 'Studentlivet',
+    href: '/info#faq',
+    description: 'Coming soon.',
+    category: 'Ny',
   },
   {
-    title: 'Student Sørøst',
-    href: '/',
-    description:
-      'Coming soon.',
+    title: 'Ofte stilte spørsmål',
+    href: '#student',
+    description: 'Coming soon.',
+    category: 'Ny',
+  },
+  {
+    title: 'Studentorganisasjoner',
+    href: '#student',
+    description: 'Coming soon.',
+    category: 'Ny',
+  },
+  {
+    title: 'Student Sørøst Appen',
+    href: '#student',
+    description: 'Coming soon.',
+    category: 'Ny',
+  },
+  {
+    title: 'Fadder',
+    href: '#component1',
+    description: 'Component 1 description.',
+    category: 'Fadder',
+  },
+  {
+    title: 'Trygg Fadder',
+    href: '#component2',
+    description: 'Component 2 description.',
+    category: 'Fadder',
+  },
+  {
+    title: 'Faddergrupper',
+    href: '#component2',
+    description: 'Component 2 description.',
+    category: 'Fadder',
   },
 ];
 
 export function Navbar() {
-  const [showMobileMenu, setShowMobileMenu] = React.useState<boolean>(false)
+  const gettingStarted = menuItems.filter(
+    item => item.category === 'Fadder',
+  );
+  const components = menuItems.filter(item => item.category === 'Ny');
+
   return (
-    <NavigationMenu className='font-poppins'>
+    <NavigationMenu className="font-poppins">
       <NavigationMenuList>
-      <NavigationMenuItem>
+        <NavigationMenuItem>
           <Link href="/program" legacyBehavior passHref>
-            <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-              Program
+            <NavigationMenuLink className={cn('bg-transparent p-3 text-sm rounded-md hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground')}>
+              <span className='font-normal'>Program</span>
             </NavigationMenuLink>
           </Link>
         </NavigationMenuItem>
         <NavigationMenuItem>
-          
-        <Link href="/fadder" legacyBehavior passHref>
-            <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-              Fadder
-            </NavigationMenuLink>
-          </Link>
+          <NavigationMenuTrigger className={cn('bg-transparent')}><span className='font-normal'>Fadder</span></NavigationMenuTrigger>
+          <NavigationMenuContent>
+            <ul className="grid w-[400px] gap-3 p-4 grid-cols-1 ">
+              {gettingStarted.map(component => (
+                <ListItem
+                  key={component.title}
+                  title={component.title}
+                  href={component.href}
+                >
+                  {component.description}
+                </ListItem>
+              ))}
+            </ul>
+          </NavigationMenuContent>
         </NavigationMenuItem>
         <NavigationMenuItem>
-        <Link href="/info" legacyBehavior passHref>
-            <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-              Ny Student
-            </NavigationMenuLink>
-          </Link>
+          <NavigationMenuTrigger className={cn('bg-transparent')}><span className='font-normal'>Ny Student</span></NavigationMenuTrigger>
+          <NavigationMenuContent>
+            <ul className="grid w-[400px] gap-3 p-4  grid-cols-1 ">
+              {components.map(component => (
+                <ListItem
+                  key={component.title}
+                  title={component.title}
+                  href={component.href}
+                >
+                  {component.description}
+                </ListItem>
+              ))}
+            </ul>
+          </NavigationMenuContent>
         </NavigationMenuItem>
       </NavigationMenuList>
     </NavigationMenu>
@@ -73,22 +123,20 @@ const ListItem = React.forwardRef<
 >(({ className, title, children, ...props }, ref) => {
   return (
     <li>
-      <NavigationMenuLink asChild>
-        <Link
-          href={props.href ?? '#'}
+      <Link href={props.href!} legacyBehavior passHref scroll {...props}>
+        <a
           ref={ref}
           className={cn(
             'block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground',
             className,
           )}
-          {...props}
         >
           <div className="text-sm font-medium leading-none">{title}</div>
           <p className="line-clamp-2 text-sm leading-snug text-muted-foreground">
             {children}
           </p>
-        </Link>
-      </NavigationMenuLink>
+        </a>
+      </Link>
     </li>
   );
 });
