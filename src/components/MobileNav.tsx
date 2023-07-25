6be5264ded3b70bd04d9bb5c5594ea9fedc0c3db
DahlGitHub <78036458+DahlGitHub@users.noreply.github.com>
2023-07-25 03:32:40 +0200

    mobile nav

--- src/components/MobileNav.tsx
+++ src/components/MobileNav.tsx
@@ -1,30 +1,65 @@
-import { cn } from "@/lib/utils";
-import Link from "next/link";
+import { cn } from '@/lib/utils';
+import { ChevronRight } from 'lucide-react';
+import Link from 'next/link';
+import { Separator } from './ui/separator';
+
+const links = [
+  {
+    title: 'Fadderuka',
+    children: [{ title: 'Program', href: '/' }],
+  },
+  {
+    title: 'Fadder',
+    children: [
+      { title: 'Min Fadder', href: '/' },
+      { title: 'Trygg Fadder', href: '/' },
+      { title: 'Faddergrupper', href: '/' },
+    ],
+  },
+  {
+    title: 'Ny Student',
+    children: [
+      { title: 'Studentlivet', href: '/' },
+      { title: 'Ofte stilte spørsmål', href: '/' },
+      { title: 'Studentorganisasjoner', href: '/' },
+      { title: 'Student Sørøst Appen', href: '/' },
+    ],
+  },
+];
 
 export function MobileNav() {
-
-    return (
-      <div
-        className={cn(
-          "fixed inset-0 top-16 z-50 grid h-[calc(100vh-4rem)] grid-flow-row auto-rows-max overflow-auto p-6 pb-32 shadow-md animate-in slide-in-from-bottom-80 md:hidden"
-        )}
-      >
-        <div className="relative z-20 grid gap-6 rounded-md bg-popover p-4 text-popover-foreground shadow-md">
-          <Link href="/" className="flex items-center space-x-2">
-            
-            <span className="font-bold">He</span>
-          </Link>
-          <nav className="grid grid-flow-row auto-rows-max text-sm">
-              <Link
-                href="/"
-                className={cn(
-                  "flex w-full items-center rounded-md p-2 text-sm font-medium hover:underline"
+  return (
+    <div
+      className={cn(
+        'fixed font-poppins inset-0 top-16 z-50 grid h-[calc(100vh-4rem)] grid-flow-row auto-rows-max overflow-auto p-2 pb-32 shadow-md animate-in slide-in-from-left-80 md:hidden',
+      )}
+    >
+      <div className="relative z-40 grid gap-6 rounded-md bg-popover p-4 text-popover-foreground shadow-md">
+        {links.map((link, index) => (
+          <div key={index}>
+            <>
+              <span className="font-semibold text-[13px] text-purple-700">{link.title}<Separator className='my-2' /></span>
+              <nav className="grid grid-flow-row auto-rows-max text-sm">
+                {link.children.map(
+                  (childLink, childIndex) =>
+                    childLink.href && (
+                      <Link
+                        key={childIndex}
+                        href={childLink.href}
+                        className={cn(
+                          'flex w-full items-center justify-between rounded-md p-2 text-sm font-medium hover:bg-slate-100',
+                        )}
+                      >
+                        <span className="mx-2">{childLink.title}</span>
+                        <ChevronRight className="text-gray-600" />
+                      </Link>
+                    ),
                 )}
-              >
-                Title
-              </Link>
-          </nav>
-        </div>
+              </nav>
+            </>
+          </div>
+        ))}
       </div>
-    )
-  }
+    </div>
+  );
+}
--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -3,34 +3,45 @@ import Footer from './Footer';
 import { Navbar } from './Navbar';
 import Link from 'next/link';
 import Image from 'next/image';
+import { MobileNav } from '../MobileNav';
+import { AlignLeft, X } from 'lucide-react';
 
 export default function Layout({ children }: { children: React.ReactNode }) {
-
-
+  const [showMobileMenu, setShowMobileMenu] = React.useState<boolean>(false);
 
   return (
     <div className="flex min-h-screen flex-col">
       <header className="sticky top-0 z-40 w-full border-b border-gray-100 backdrop-blur bg-white/80">
         <div className="container flex h-14 items-center space-x-4 sm:justify-between sm:space-x-0">
-          
           <Link href="/">
-              <Image 
-              src="https://firebasestorage.googleapis.com/v0/b/fadderukabo.appspot.com/o/USN-symbol_lilla.png?alt=media&token=0ab4e9b3-3974-41f4-8d7f-41a0753bed07" 
+            <Image
+              src="https://firebasestorage.googleapis.com/v0/b/fadderukabo.appspot.com/o/USN-symbol_sort.png?alt=media&token=e72960d6-666d-4c3c-9b6a-a38d9ab25ce7"
               alt={'USN'}
               width={40}
-              height={40} />
+              height={40}
+            />
           </Link>
           <div className="flex flex-1 items-center space-x-4 sm:justify-end">
             <div className="flex-1 sm:grow-0">
-              <Navbar />
+              <nav className="hidden md:flex">
+                <Navbar />
+              </nav>
+              <div className='flex justify-end'>
+              <button
+                className="flex items-center space-x-2 md:hidden"
+                onClick={() => setShowMobileMenu(!showMobileMenu)}
+              >
+                {showMobileMenu ? <X/> : <AlignLeft/>}
+              </button>
+              </div>
+              {showMobileMenu && <MobileNav />}
+              
             </div>
           </div>
         </div>
       </header>
-        <main className="mb-auto">
-          {children}
-        </main>
-        <Footer />
-      </div>
+      <main className="mb-auto">{children}</main>
+      <Footer />
+    </div>
   );
 }
