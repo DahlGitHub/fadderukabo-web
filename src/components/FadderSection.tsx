173057ce6b20fb4ae659e85436d7448fde1fc9d0
DahlGitHub <78036458+DahlGitHub@users.noreply.github.com>
2023-08-03 05:29:01 +0200

    fadder section and tabs

--- src/components/FadderSection.tsx
+++ src/components/FadderSection.tsx
@@ -5,14 +5,21 @@ export const FadderSection = () => {
     <div className="container py-5 pb-5 justify-center relative">
       <div
         aria-hidden="true"
-        className="absolute z-10 inset-0 top-60 grid grid-cols-2 -space-x-52 opacity-50 dark:opacity-30"
+        className="absolute z-0 inset-0 top-60 grid grid-cols-2 -space-x-52 opacity-50 dark:opacity-30"
       >
-        <div className="h-60 z-10 bg-gradient-to-br from-primary to-red-400 blur-[106px]"></div>
-        <div className="h-40 z-10 bg-gradient-to-r from-red-900 to-orange-200 blur-[106px]"></div>
+        <div className="h-60 z-0 bg-gradient-to-br from-primary to-red-400 blur-[106px]"></div>
+        <div className="h-40 z-0 bg-gradient-to-r from-red-900 to-orange-200 blur-[106px]"></div>
       </div>
-      <div className="flex flex-col md:flex-row z-50">
-        <div className="md:w-1/2">
-            <div>Title</div>
+      <div className="flex flex-col md:flex-row z-40">
+        <div className="md:w-1/2 my-auto">
+            <div className='font-poppins flex flex-col container'>
+            <span className="font-semibold text-2xl text-red-400">
+            Campus Bø
+          </span>
+          <span className="text-4xl md:text-5xl pb-3 font-bold leading-tight tracking-tight text-gray-900 dark:text-white">
+            Faddere for studenter av studenter. Bli en del av noe større.
+          </span>
+            </div>
         </div>
         <div className="md:w-1/2">
           <img src="https://cdn.discordapp.com/attachments/1075240291226890384/1136097503436079194/Component_115.png" />
--- src/components/FadderTabs.tsx
+++ src/components/FadderTabs.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { Heart } from 'lucide-react';
+import { HeartHandshake, ShieldCheck, Users2 } from 'lucide-react';
 import { Tabs, TabsContent, TabsList, TabsTrigger } from './ui/tabs';
 
 const pointsData = [
@@ -7,24 +7,27 @@ const pointsData = [
     value: '1',
     title: 'Rollen som fadder',
     text: 'En fadder ved USN fungerer som en bro mellom de nye studentene og studentmiljøet på campus. Fadderen skal ta imot nye studenter og være med på å gi deg en god start på studiehverdagen.',
-    icon: <Heart size={40} />,
-    image: 'https://cdn.discordapp.com/attachments/1075240291226890384/1136106171187601408/299775273_5654008931305650_2111197190592147934_n.jpg',
+    icon: <HeartHandshake size={32} strokeWidth={1.5} />,
+    image:
+      'https://cdn.discordapp.com/attachments/1075240291226890384/1136368036119588864/Component_1161.png',
     header: 'Rolle',
   },
   {
     value: '2',
-    title: 'Oppgaver som fadder',
+    title: 'Nati er fuckings gay og gravid',
     text: 'Some decent text for the second point',
-    icon: <Heart size={40} />,
-    image: 'https://link-to-second-image.com',
+    icon: <ShieldCheck size={32} strokeWidth={1.5} />,
+    image:
+      'https://cdn.discordapp.com/attachments/1075240291226890384/1136368035343646884/Component_1171.png',
     header: 'Oppgave',
   },
   {
     value: '3',
     title: 'Faddergruppene',
     text: 'Some decent text for the third point',
-    icon: <Heart size={40} />,
-    image: 'https://link-to-third-image.com',
+    icon: <Users2 size={32} strokeWidth={1.5} />,
+    image:
+      'https://cdn.discordapp.com/attachments/1075240291226890384/1136368035654013059/Component_1181.png',
     header: 'Gruppe',
   },
 ];
@@ -37,43 +40,47 @@ export const FadderTabs = () => {
   };
 
   return (
-    <div className="container py-5 pb-5 justify-center">
-      <div className="flex flex-col font-poppins">
+    <div className="container py-5 pb-5">
+      <div className="flex flex-col font-poppins items-center">
         <span className="font-semibold text-2xl text-red-400">01.</span>
         <span className="text-4xl font-bold text-gray-900">
           Fadderordningen
         </span>
       </div>
       <Tabs defaultValue={pointsData[0].value} className="w-full my-5">
-        <TabsList className="flex-row inline-grid gap-x-2 px-4 sm:px-0 xl:gap-x-6">
-          <ul className="grid grid-cols-3">
-            {pointsData.map((point, index) => (
-              <TabsTrigger
-                key={index}
-                value={point.value}
-                onClick={() => onTriggerClick(point.value)}
-              >
-                <li>
-                  <button
-                    type="button"
-                    className={`group font-poppins text-sm font-semibold w-full flex flex-col items-center ${
-                      activeTab === point.value ? 'text-red-400' : ''
-                    }`}
-                  >
-                    <span className="mb-4">
-                      <Heart size={40} />
-                    </span>
-                    <span>{point.header}</span>
-                  </button>
-                </li>
-              </TabsTrigger>
-            ))}
-          </ul>
-        </TabsList>
+        <div className="flex justify-center mx-auto">
+          <TabsList className="inline-grid grid">
+            <ul className="grid grid-cols-3">
+              {pointsData.map((point, index) => (
+                <TabsTrigger
+                  key={index}
+                  value={point.value}
+                  onClick={() => onTriggerClick(point.value)}
+                >
+                  
+                    <div
+                      className={`group font-poppins text-sm font-semibold w-full flex flex-col justify-center items-center ${
+                        activeTab === point.value ? 'text-red-400' : 'hover:text-gray-600'
+                      }`}
+                    >
+                      <span className="mb-4">
+                        {point.icon}
+                      </span>
+                      <span>{point.header}</span>
+                    </div>
+                 
+                </TabsTrigger>
+              ))}
+            </ul>
+          </TabsList>
+        </div>
 
         {pointsData.map((point, index) => (
           <TabsContent key={index} value={point.value}>
-            <div className="flex flex-col md:flex-row mt-10">
+            <div className="flex flex-col md:flex-row mt-10 max-w-5xl mx-auto items-center">
+              <div className="md:w-1/2">
+                <img className="rounded-lg" src={point.image} />
+              </div>
               <div className="md:w-1/2">
                 <div className="pb-5 container flex flex-col font-poppins">
                   <span className="font-semibold text-xl text-red-400">
@@ -83,9 +90,6 @@ export const FadderTabs = () => {
                     <p>{point.text}</p>
                   </div>
                 </div>
-              </div>
-              <div className="md:w-1/2">
-                <img className="rounded-lg" src={point.image} />
               </div>
             </div>
           </TabsContent>
